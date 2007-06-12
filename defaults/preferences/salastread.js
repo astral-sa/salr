cf3248fdde2aae970260fa8446339c5bd6aca10e
duz <duz@salr.firefox>
2007-06-12 05:33:35 +0000

    Nightly

--- defaults/preferences/salastread.js
+++ defaults/preferences/salastread.js
@@ -1,4 +1,4 @@
-pref('extensions.salastread.currentVersion', '1.99.070606');
+pref('extensions.salastread.currentVersion', '1.99.070611');
 pref('extensions.salastread.disabled', false);
 pref('extensions.salastread.postsPerPage', 40);
 pref('extensions.salastread.enableFYAD', true);
