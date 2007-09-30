bd37fbf767cfca9d4c05791758abbd6df9caed96
duz <duz@salr.firefox>
2007-09-30 02:58:09 +0000

    Cleaning up the code a bit

--- components/SALastReadCom.js
+++ components/SALastReadCom.js
@@ -473,7 +473,7 @@ salrPersistObject.prototype = {
 		return admins;
 	},
 
-	// Return a string that contains a CSS file with the custom colors set (when enabled)
+	// Return a string that contains CSS instructions for our settings
 	get generateDynamicCSS()
 	{
 		var CSSFile = '';
@@ -800,12 +800,14 @@ salrPersistObject.prototype = {
 	// Gets a username from the DB
 	// @param: (int) User ID
 	// @return: (string) username or (null) if not found
-	getUserName: function(userid) {
+	getUserName: function(userid)
+	{
 		var statement = this.database.createStatement("SELECT `username` FROM `userdata` WHERE `userid` = ?1");
 		statement.bindInt32Parameter(0, userid);
 		var foundusername = null;
-		if (statement.executeStep()) {
-		  foundusername = statement.getString(0);
+		if (statement.executeStep())
+		{
+			foundusername = statement.getString(0);
 		}
 		statement.reset();
 		return foundusername;
@@ -1589,110 +1591,108 @@ salrPersistObject.prototype = {
 		pageList = pageList[pageList.length-1];
 		var numPages = pageList.innerHTML.match(/\((\d+)\)/);
 		var curPage = this.selectSingleNode(doc, doc, "//SPAN[contains(@class,'curpage')]");
-		if (pageList.childNodes.length > 1) // Are there pages
+		if (pageList.childNodes.length <= 1)
 		{
-			numPages = parseInt(numPages[1], 10);
-			curPage = parseInt(curPage.innerHTML, 10);
-			var navDiv = doc.createElement("div");
-			navDiv.className = "salastread_pagenavigator";
-			var firstButtonImg = doc.createElement("img");
-			firstButtonImg.title = "Go to First Page";
-			firstButtonImg.src = "chrome://salastread/skin/nav-firstpage.png";
-			var prevButtonImg = doc.createElement("img");
-			prevButtonImg.title = "Go to Previous Page";
-			prevButtonImg.src = "chrome://salastread/skin/nav-prevpage.png";
-			if (curPage == 1)
-			{
-				firstButtonImg.className = "disab";
-				navDiv.appendChild(firstButtonImg);
-				prevButtonImg.className = "disab";
-				navDiv.appendChild(prevButtonImg);
-			}
-			else
-			{
-				var firstButton = doc.createElement("a");
-				firstButton.href = this.editPageNumIntoURI(doc, "pagenumber=1");
-				firstButton.appendChild(firstButtonImg);
-				navDiv.appendChild(firstButton);
-				var prevButton = doc.createElement("a");
-				prevButton.href = this.editPageNumIntoURI(doc, "pagenumber=" + (curPage-1));
-				prevButton.appendChild(prevButtonImg);
-				navDiv.appendChild(prevButton);
-			}
-			var pageSel = doc.createElement("select");
-			pageSel.size = 1;
-			for (var pp=1; pp<=numPages; pp++)
-			{
-				var topt = doc.createElement("option");
-				topt.appendChild(doc.createTextNode(pp));
-				topt.value = pp;
-				if (pp==curPage) topt.selected = true;
-				pageSel.appendChild(topt);
-			}
-			if (curPage == 1)
-			{
-				pageSel.onchange = function() { doc.location = doc.baseURI + "&pagenumber="+this.value; };
-			}
-			else
-			{
-				pageSel.onchange = function() {
-					if (doc.location.pathname == "/showthread.php")
-					{
-						var threadid = doc.evaluate("//DIV[contains(@class,'pages')]//A[contains(@href,'threadid=')]", doc, null, 9, null).singleNodeValue.href.match(/threadid=(\d+)/i)[1];
-						doc.location = doc.location.pathname+"?threadid="+threadid+"&pagenumber="+this.value;
-					}
-					else
-					{
-						doc.location = doc.baseURI.replace(/pagenumber=(\d+)/, "pagenumber="+this.value);
-					}
-				};
-			}
-			navDiv.appendChild(pageSel);
-			var nextButtonImg = doc.createElement("img");
-			nextButtonImg.title = "Go to Next Page";
-			nextButtonImg.src = "chrome://salastread/skin/nav-nextpage.png";
-			var lastButtonImg = doc.createElement("img");
-			lastButtonImg.title = "Go to Last Page";
-			lastButtonImg.src = "chrome://salastread/skin/nav-lastpage.png";
-			if (curPage == numPages)
-			{
-				nextButtonImg.className = "disab";
-				navDiv.appendChild(nextButtonImg);
-				lastButtonImg.className = "disab";
-				navDiv.appendChild(lastButtonImg);
-			}
-			else
-			{
-				var nextButton = doc.createElement("a");
-				nextButton.href = this.editPageNumIntoURI(doc, "pagenumber=" + (curPage+1));
-				nextButton.appendChild(nextButtonImg);
-				navDiv.appendChild(nextButton);
-				var lastButton = doc.createElement("a");
-				lastButton.href = this.editPageNumIntoURI(doc, "pagenumber=" + numPages);
-				lastButton.appendChild(lastButtonImg);
-				navDiv.appendChild(lastButton);
-			}
-			if (doc.location.pathname == "/showthread.php")
-			{
-				var lastButtonImg = doc.createElement("img");
-				lastButtonImg.title = "Go to First Unread Post";
-				lastButtonImg.src = "chrome://salastread/skin/lastpost.png";
-				var lastButton = doc.createElement("a");
-				lastButton.href = this.editPageNumIntoURI(doc, "goto=newpost");
-				lastButton.appendChild(lastButtonImg);
-				navDiv.appendChild(lastButton);
-			}
-			if (!this.getPreference("enableForumNavigator") && !this.getPreference("enablePageNavigator"))
-			{
-				navDiv.style.visibility = 'hidden';
-				navDiv.style.display = 'none';
-			}
-			doc.body.appendChild(navDiv);
+			// There's only one page
+			return;
+		}
+		numPages = parseInt(numPages[1], 10);
+		curPage = parseInt(curPage.innerHTML, 10);
+		var navDiv = doc.createElement("div");
+		navDiv.className = "salastread_pagenavigator";
+		var firstButtonImg = doc.createElement("img");
+		firstButtonImg.title = "Go to First Page";
+		firstButtonImg.src = "chrome://salastread/skin/nav-firstpage.png";
+		var prevButtonImg = doc.createElement("img");
+		prevButtonImg.title = "Go to Previous Page";
+		prevButtonImg.src = "chrome://salastread/skin/nav-prevpage.png";
+		if (curPage == 1)
+		{
+			firstButtonImg.className = "disab";
+			navDiv.appendChild(firstButtonImg);
+			prevButtonImg.className = "disab";
+			navDiv.appendChild(prevButtonImg);
 		}
 		else
 		{
-			numPages = 1;
+			var firstButton = doc.createElement("a");
+			firstButton.href = this.editPageNumIntoURI(doc, "pagenumber=1");
+			firstButton.appendChild(firstButtonImg);
+			navDiv.appendChild(firstButton);
+			var prevButton = doc.createElement("a");
+			prevButton.href = this.editPageNumIntoURI(doc, "pagenumber=" + (curPage-1));
+			prevButton.appendChild(prevButtonImg);
+			navDiv.appendChild(prevButton);
 		}
+		var pageSel = doc.createElement("select");
+		pageSel.size = 1;
+		for (var pp=1; pp<=numPages; pp++)
+		{
+			var topt = doc.createElement("option");
+			topt.appendChild(doc.createTextNode(pp));
+			topt.value = pp;
+			if (pp==curPage) topt.selected = true;
+			pageSel.appendChild(topt);
+		}
+		if (curPage == 1)
+		{
+			pageSel.onchange = function() { doc.location = doc.baseURI + "&pagenumber="+this.value; };
+		}
+		else
+		{
+			pageSel.onchange = function() {
+				if (doc.location.pathname == "/showthread.php")
+				{
+					var threadid = doc.evaluate("//DIV[contains(@class,'pages')]//A[contains(@href,'threadid=')]", doc, null, 9, null).singleNodeValue.href.match(/threadid=(\d+)/i)[1];
+					doc.location = doc.location.pathname+"?threadid="+threadid+"&pagenumber="+this.value;
+				}
+				else
+				{
+					doc.location = doc.baseURI.replace(/pagenumber=(\d+)/, "pagenumber="+this.value);
+				}
+			};
+		}
+		navDiv.appendChild(pageSel);
+		var nextButtonImg = doc.createElement("img");
+		nextButtonImg.title = "Go to Next Page";
+		nextButtonImg.src = "chrome://salastread/skin/nav-nextpage.png";
+		var lastButtonImg = doc.createElement("img");
+		lastButtonImg.title = "Go to Last Page";
+		lastButtonImg.src = "chrome://salastread/skin/nav-lastpage.png";
+		if (curPage == numPages)
+		{
+			nextButtonImg.className = "disab";
+			navDiv.appendChild(nextButtonImg);
+			lastButtonImg.className = "disab";
+			navDiv.appendChild(lastButtonImg);
+		}
+		else
+		{
+			var nextButton = doc.createElement("a");
+			nextButton.href = this.editPageNumIntoURI(doc, "pagenumber=" + (curPage+1));
+			nextButton.appendChild(nextButtonImg);
+			navDiv.appendChild(nextButton);
+			var lastButton = doc.createElement("a");
+			lastButton.href = this.editPageNumIntoURI(doc, "pagenumber=" + numPages);
+			lastButton.appendChild(lastButtonImg);
+			navDiv.appendChild(lastButton);
+		}
+		if (doc.location.pathname == "/showthread.php")
+		{
+			var lastButtonImg = doc.createElement("img");
+			lastButtonImg.title = "Go to First Unread Post";
+			lastButtonImg.src = "chrome://salastread/skin/lastpost.png";
+			var lastButton = doc.createElement("a");
+			lastButton.href = this.editPageNumIntoURI(doc, "goto=newpost");
+			lastButton.appendChild(lastButtonImg);
+			navDiv.appendChild(lastButton);
+		}
+		if (!this.getPreference("enableForumNavigator") && !this.getPreference("enablePageNavigator"))
+		{
+			navDiv.style.visibility = 'hidden';
+			navDiv.style.display = 'none';
+		}
+		doc.body.appendChild(navDiv);
 	},
 
 	// Helper function for addPagination()
