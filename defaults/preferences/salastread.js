59ac719896db57952f7628c6a180650727ee158c
duz <duz@salr.firefox>
2008-06-19 03:49:05 +0000

    Update version number Build 1.99.080618

--- defaults/preferences/salastread.js
+++ defaults/preferences/salastread.js
@@ -1,4 +1,4 @@
-pref('extensions.salastread.currentVersion', '1.99.080610');
+pref('extensions.salastread.currentVersion', '1.99.080618');
 pref('extensions.salastread.disabled', false);
 pref('extensions.salastread.postsPerPage', 40);
 pref('extensions.salastread.enableFYAD', true);
