637f52f83b53318cd56748c5dc1daba0f3906913
duz <duz@salr.firefox>
2008-11-09 05:57:08 +0000

    Pushing out build 1.99.081108

--- defaults/preferences/salastread.js
+++ defaults/preferences/salastread.js
@@ -1,4 +1,4 @@
-pref('extensions.salastread.currentVersion', '1.99.080810');
+pref('extensions.salastread.currentVersion', '1.99.081108');
 pref('extensions.salastread.disabled', false);
 pref('extensions.salastread.postsPerPage', 40);
 pref('extensions.salastread.enableFYAD', true);
