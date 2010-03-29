149cf6682b0ba3ea5510a650c40dc00d931d6f5e
astral <astralsalr@gmail.com>
2010-03-29 05:26:52 +0000

    Version bump + claim to be compatible with Firefox 3.7

--- defaults/preferences/salastread.js
+++ defaults/preferences/salastread.js
@@ -1,4 +1,4 @@
-pref('extensions.salastread.currentVersion', '1.99.100326');
+pref('extensions.salastread.currentVersion', '1.99.100328');
 pref('extensions.salastread.disabled', false);
 pref('extensions.salastread.postsPerPage', 40);
 pref('extensions.salastread.enableFYAD', true);
