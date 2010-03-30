5e5ea47abfae6c8d141025a6abd01f64235922a8
astral <astralsalr@gmail.com>
2010-03-30 03:52:14 +0000

    Uploading version 1.99.100329

--- defaults/preferences/salastread.js
+++ defaults/preferences/salastread.js
@@ -1,4 +1,4 @@
-pref('extensions.salastread.currentVersion', '1.99.100328');
+pref('extensions.salastread.currentVersion', '1.99.100329');
 pref('extensions.salastread.disabled', false);
 pref('extensions.salastread.postsPerPage', 40);
 pref('extensions.salastread.enableFYAD', true);
