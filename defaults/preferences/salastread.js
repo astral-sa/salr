326d779f9691eccb7dbdb9decbbeef7cb228422f
astral <astralsalr@gmail.com>
2010-03-31 02:33:31 +0000

    Version bump for 1.99.100330

--- defaults/preferences/salastread.js
+++ defaults/preferences/salastread.js
@@ -1,4 +1,4 @@
-pref('extensions.salastread.currentVersion', '1.99.100329');
+pref('extensions.salastread.currentVersion', '1.99.100330');
 pref('extensions.salastread.disabled', false);
 pref('extensions.salastread.postsPerPage', 40);
 pref('extensions.salastread.enableFYAD', true);
