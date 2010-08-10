f154cbe3684a5e09b9bc66b3e223c7af394731ef
astral <astralsalr@gmail.com>
2010-08-10 01:24:04 +0000

    Added "Select All Threads" button to "Ignored" prefpane; updated wording a bit.

--- chrome/content/salastread/pref/pref-ignored.js
+++ chrome/content/salastread/pref/pref-ignored.js
@@ -1,69 +1,100 @@
 var persistObject;
 
 //general init
-function ignoreInit() {
+function ignoreInit()
+{
 	persistObject = Components.classes['@evercrest.com/salastread/persist-object;1']  
 					.getService().wrappedJSObject;
-	
+
 	updateIgnoredThreadList();
 }
 
 //this is only ever called once but eh
-function updateIgnoredThreadList() {
+function updateIgnoredThreadList()
+{
 	var list = document.getElementById("ignored_thread_list");
 	while (list.firstChild)
 		list.removeChild(list.firstChild);
-	
+
 	var threads = persistObject.ignoreList;
-	for(var id in threads) {
+	for(var id in threads)
+	{
 		var title = threads[id];
-		
 		addThreadToList(id, title);
 	}
+
+	// Disable 'Select all' button if the list is empty
+	var selbtn = document.getElementById("unignore_selectall_button");
+	if (list.firstChild)
+		selbtn.setAttribute("disabled", false);
+	else
+		selbtn.setAttribute("disabled", true);
 }
 
 //adds a thread to the list
-function addThreadToList(id, title) {
+function addThreadToList(id, title)
+{
 	var list = document.getElementById("ignored_thread_list");
-	
+
 	var li = document.createElement("listitem");
 		li.setAttribute("label", "[" + id + "] " + title);
 		li.setAttribute("threadtitle", title);
 		li.setAttribute("threadid", id);
-	
-	for(var i = (list.childNodes.length - 1); i >= 0 ; i--) {
+
+	for (var i = (list.childNodes.length - 1); i >= 0 ; i--)
+	{
 		var tchild = list.childNodes[i];
 		var tctitle = tchild.getAttribute("threadtitle");
-		if (tctitle > title) {
+		if (tctitle > title)
+		{
 			list.insertBefore(li, tchild);
 			return;
 		}
 	}
-	
+
 	list.appendChild(li);
 }
 
 //enables or disables the remove button
-function listSelect() {
+function listSelect()
+{
 	var list = document.getElementById("ignored_thread_list");
 	var btn = document.getElementById("unignore_thread_button");
-	
-	if(list.selectedItems.length) {
+	var selbtn = document.getElementById("unignore_selectall_button");
+
+	if (list.selectedItems.length)
+	{
 		btn.setAttribute("disabled", false);
-	} else {
-		btn.setAttribute("disabled", true);
+
+		// Update button text
+		if (list.selectedItems.length > 1)
+			btn.setAttribute("label", "Unignore Selected Threads");
+		else
+			btn.setAttribute("label", "Unignore Selected Thread");
 	}
+	else
+		btn.setAttribute("disabled", true);
+
+	// Disable 'Select all' button if this results in an empty list
+	if (!list.firstChild)
+		selbtn.setAttribute("disabled", true);
 }
 
 //toggles ignore status on all selected threads when the button is set
-function unignoreThreads() {
+function unignoreThreads()
+{
 	var list = document.getElementById("ignored_thread_list");
 	var items = list.selectedItems;
-	
+
 	for(var i = (items.length - 1); i >= 0; i--) {
 		var item = items[i];
-		
 		persistObject.toggleThreadIgnore(item.getAttribute("threadid"));
 		list.removeChild(item);
 	}
+}
+
+function selectAllThreads()
+{
+	var list = document.getElementById("ignored_thread_list");
+	list.selectAll();
 }
