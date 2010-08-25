c7b33170f61d4db96c6f846d87e3fe1438fbfe49
astral <astralsalr@gmail.com>
2010-08-25 09:21:55 +0000

    Users prefpane now has a nifty little panel for editing a user's colors and note without having to access it all through contextual menu items. The contextual menu items still work, and everything should play nicely.
    Also made the add coloring / add user bit make sure to set a custom note so the target user wouldn't be lost track of immediately if no values were changed.

--- chrome/content/salastread/pref/pref-users.js
+++ chrome/content/salastread/pref/pref-users.js
@@ -27,16 +27,21 @@ function initUsers()
 			if (!persistObject.userExists(userid))
 			{
 				persistObject.addUser(userid, username);
+				persistObject.setPosterNotes(userid, "New User");
 				addListUser(listBox, userid, username);
 			}
 			else
 			{
 				var udata = persistObject.isUserIdColored(userid);
 				if (udata.color == 0 && udata.background == 0 && !persistObject.getPosterNotes(userid))
+				{
+					persistObject.setPosterNotes(userid, "New User");
 					addListUser(listBox, userid, username);
+				}
 			}
 		}
 	}
+	updateCustomizePane();
 }
 
 //add a user to the list box with proper coloring
@@ -105,7 +110,6 @@ function changeColor(type)
 			}
 
 			li.childNodes[1].style.color = value;
-
 			persistObject.setPosterColor(li.value, obj.value);
 		}
 		else
@@ -116,9 +120,9 @@ function changeColor(type)
 			}
 
 			li.childNodes[1].style.backgroundColor = value;
-
 			persistObject.setPosterBackground(li.value, obj.value);
 		}
+		updateCustomizePane();
 	}
 }
 
@@ -161,7 +165,10 @@ function addUser()
 					}
 				}
 				if (foundit == false)
+				{
+					persistObject.setPosterNotes(text.value, "New User");
 					addListUser(listBox, text.value, udata.username);
+				}
 				else
 					alert("Error: User already has custom highlighting.");
 			}
@@ -206,6 +213,104 @@ function editNote()
 	{
 		persistObject.setPosterNotes(li.value, text.value);
 		li.childNodes[2].setAttribute("label", text.value);
+		updateCustomizePane();
 	}
 }
 
+// Called when listbox selection is updated or if we need to manually update
+function updateCustomizePane()
+{
+	try
+	{
+		var sellist = document.getElementById("userColoring").selectedItems;
+		// Just allow one for now.
+		if (sellist)
+		{
+			sellist = sellist[0];
+		}
+		if (sellist)
+		{
+			var children = document.getElementById("customizevbox").getElementsByTagName('*');
+			for (var i = 0; i < children.length; i++)
+				children[i].disabled = false;
+			document.getElementById("deleteuserbutton").disabled = false;
+			var cusername = sellist.childNodes[1].getAttribute("label");
+			var cnote = sellist.childNodes[2].getAttribute("label");
+			var cuidstring = ' (' + sellist.childNodes[0].getAttribute("label") + ')';
+			document.getElementById("fgthumbnail").style.backgroundColor = sellist.childNodes[1].style.color;
+			document.getElementById("bgthumbnail").style.backgroundColor = sellist.childNodes[1].style.backgroundColor;
+			if (cnote)
+				cnote = cnote.replace(/(?:<br \/>)/g, "\n");
+			document.getElementById("usernotebox").value = cnote;
+			if (cusername && cusername != 'null' && cusername != 'Unknown')
+			{
+				cuidstring = '';
+			}
+			else
+			{
+				cusername = 'unknown user';
+			}
+			document.getElementById("customizelabel").setAttribute("value", cusername + cuidstring);
+		}
+		else
+		{
+			// Nothing selected or empty list
+			var children = document.getElementById("customizevbox").getElementsByTagName('*');
+			for (var i = 0; i < children.length; i++)
+				children[i].disabled = true;
+			document.getElementById("deleteuserbutton").disabled = true;
+			document.getElementById("fgthumbnail").style.backgroundColor = 'transparent';
+			document.getElementById("bgthumbnail").style.backgroundColor = 'transparent';
+			document.getElementById("usernotebox").value = '';
+			document.getElementById("customizelabel").setAttribute("value", "Select a user to customize");
+		}
+	}
+	catch(e)
+	{ 
+		alert("users select error: " + e); 
+	}
+}
+
+function colorClicked(el, ctype)
+{
+	var li = document.getElementById("userColoring").selectedItem;
+	if (!li)
+		return;
+	var pobj = new Object();
+	if (ctype === 'fgcolor')
+		pobj.value = persistObject.getPosterColor(li.value);
+	else
+		pobj.value = persistObject.getPosterBackground(li.value);
+	pobj.accepted = false;
+	window.openDialog('chrome://salastread/content/colorpicker/colorpickerdialog.xul', 'colorpickerdialog', 'modal,chrome',pobj);
+	if (pobj.accepted)
+	{
+		var friendlyvalue = pobj.value;
+		if (pobj.value == 0 && ctype == 'bgcolor')
+		{
+			friendlyvalue = 'transparent';
+		}
+		el.style.backgroundColor = friendlyvalue;
+		if (ctype == 'fgcolor')
+		{
+			li.childNodes[1].style.color = friendlyvalue;
+			persistObject.setPosterColor(li.value, pobj.value);
+		}
+		else
+		{
+			li.childNodes[1].style.backgroundColor = friendlyvalue;
+			persistObject.setPosterBackground(li.value, pobj.value);
+		}
+	}
+}
+
+function updateNote()
+{
+	var li = document.getElementById("userColoring").selectedItem;
+	if (!li)
+		return;
+	var newnotetext = document.getElementById("usernotebox").value;
+	newnotetext = newnotetext.replace(/(?:\n)/g, "<br />");
+	persistObject.setPosterNotes(li.value, newnotetext);
+	li.childNodes[2].setAttribute("label", newnotetext);
+}
