ffc535a789ce370c097904683a1ac827e84cf352
astral <astralsalr@gmail.com>
2011-06-21 17:50:07 +0000

    Claim to support Firefox 5.0.

--- defaults/preferences/salastread.js
+++ defaults/preferences/salastread.js
@@ -1,4 +1,4 @@
-pref('extensions.salastread.currentVersion', '1.99.101123');
+pref('extensions.salastread.currentVersion', '1.99.110621');
 pref('extensions.salastread.disabled', false);
 pref('extensions.salastread.postsPerPage', 40);
 pref('extensions.salastread.enableFYAD', true);
