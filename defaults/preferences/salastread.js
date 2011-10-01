c784b9e4a2398f86ad5a62f0d1f3b38bad2df974
astral <astralsalr@gmail.com>
2011-10-01 04:05:11 +0000

    maxversion bump.

--- defaults/preferences/salastread.js
+++ defaults/preferences/salastread.js
@@ -1,4 +1,4 @@
-pref('extensions.salastread.currentVersion', '1.99.110818');
+pref('extensions.salastread.currentVersion', '1.99.110930');
 pref('extensions.salastread.disabled', false);
 pref('extensions.salastread.postsPerPage', 40);
 pref('extensions.salastread.enableFYAD', true);
