f68089c804599b8f7948faea0d812886160eeafd
astral <astralsalr@gmail.com>
2012-01-12 12:16:40 +0000

    Version 1.99.120111

--- defaults/preferences/salastread.js
+++ defaults/preferences/salastread.js
@@ -1,4 +1,4 @@
-pref('extensions.salastread.currentVersion', '1.99.111114');
+pref('extensions.salastread.currentVersion', '1.99.120111');
 pref('extensions.salastread.disabled', false);
 pref('extensions.salastread.postsPerPage', 40);
 pref('extensions.salastread.enableFYAD', true);
