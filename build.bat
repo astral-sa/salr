1b00909c6ffe441dc14e5e5f57205dadda0eeb9d
astral <astralsalr@gmail.com>
2014-02-07 03:07:41 -0800

    Update build scripts et al. for github

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
 set PATH=%PATH%;C:\Program Files\7-Zip
 
 del salastread.xpi
-7z a -tzip salastread.xpi * -r -mx=9 -xr!.svn -x!*.bat -x!*.sh -x!*.xpi -x!Thumbs.db -x!desktop.ini -x!salr.rdf
+7z a -tzip salastread.xpi * -r -mx=9 -xr!.git -x!.gitignore -x!*.bat -x!*.sh -x!*.xpi -x!Thumbs.db -x!desktop.ini -x!salr.rdf
