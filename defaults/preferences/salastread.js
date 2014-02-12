caf94b363fbe0c414c66e0adca51d7e371bda534
astral <astralsalr@gmail.com>
2014-02-12 01:29:48 -0800

    Pushing version 1.99.140211

--- defaults/preferences/salastread.js
+++ defaults/preferences/salastread.js
@@ -1,4 +1,4 @@
-pref('extensions.salastread.currentVersion', '1.99.140210');
+pref('extensions.salastread.currentVersion', '1.99.140211');
 pref('extensions.salastread.disabled', false);
 pref('extensions.salastread.postsPerPage', 40);
 pref('extensions.salastread.enableFYAD', true);
