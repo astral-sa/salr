44be065f778b75f537ace633469155e7235a5481
astral <astralsalr@gmail.com>
2015-04-13 02:07:50 -0700

    webm/gifv embedding work + adjust yt player sizes (height+5)

--- components/SALastReadCom.js
+++ components/SALastReadCom.js
@@ -19,108 +19,160 @@ function SALR_vidClick(e)
 	}
 
 	//figure out the video type
-	var videoId, videoSrc, videoTLD, yt_subd, yt_starttime, yt_start;
+	var videoId, videoType, videoTLD, yt_subd, yt_starttime, yt_start;
 	//var videoIdSearch = link.href.match(/^http\:\/\/((?:www|[a-z]{2})\.)?youtube\.com\/watch\?v=([-_0-9a-zA-Z]+)/);
 	var videoIdSearch = link.href.match(/^https?\:\/\/((?:www|[a-z]{2})\.)?(?:youtube\.com\/watch\?v=|youtu\.be\/)([-_0-9a-zA-Z]+)(?:(?:.*)?t=(?:(\d*)m)?(?:(\d*)s)?)?/);
 	if (videoIdSearch)
 	{
 		yt_subd = (videoIdSearch[1] == null ? "www." : videoIdSearch[1]);
 		videoId = videoIdSearch[2];
-		videoSrc = "youtube";
+		videoType = "youtube";
 		yt_starttime = (videoIdSearch[3] == null ? 0 : parseInt(videoIdSearch[3])) * 60 + (videoIdSearch[4] == null ? 0 : parseInt(videoIdSearch[4]));
 		yt_start = yt_starttime == 0 ? '' : 'start=' + yt_starttime;
 	}
 	else
 	{
-		var match = link.href.match(/^http\:\/\/video\.google\.c(om|a|o\.uk)\/videoplay\?docid=([-0-9]+)/);
-		videoTLD = match[1];
-		videoId = match[2];
-		videoSrc = "google";
+		var matchGV = link.href.match(/^http\:\/\/video\.google\.c(om|a|o\.uk)\/videoplay\?docid=([-0-9]+)/);
+		if (matchGV)
+		{
+			videoTLD = matchGV[1];
+			videoId = matchGV[2];
+			videoType = "google";
+		}
+		else
+		{
+			var matchGifv = (link.href.match(/^https?\:\/\/i\.imgur\.com\/(.)+\.gifv$/i));
+			// Handle imgur gifv
+			if (matchGifv && matchGifv[1])
+			{
+				videoType = 'gifv';
+				videoId = matchGifv[1];
+			}
+			// Handle WebM
+			else if (link.href.match(/^https?\:\/\/(?:.)+\.webm$/i))
+			{
+				videoType = 'webm';
+			}
+		}
 	}
 
-	//create the embedded elements (p containing video for linebreaky goodness)
-	var doc = e.originalTarget.ownerDocument;
-	var p = doc.createElement("p");
-	switch (videoSrc)
+	if (videoType)
 	{
-		case "google":
-			var embed = doc.createElement("EMBED");
-			embed.setAttribute('width', 450);
-			embed.setAttribute('height', 370);
-			embed.setAttribute('type', "application/x-shockwave-flash");
-			embed.setAttribute('class', 'salr_video');
-			embed.setAttribute('id', videoId);
-			embed.setAttribute('flashvars', '');
-			embed.setAttribute('src', 'http://video.google.c' + videoTLD + '/googleplayer.swf?docId=' + videoId + '&hl=en&fs=true');
-			embed.setAttribute('allowfullscreen', "true");
-			p.appendChild(embed);
-			break;
-		case "youtube":
-			// Figure out quality and size to use
-			var vidqual = PersistObject.getPreference("videoEmbedQuality");
-			var qualstring = '';
-			if (vidqual == "hd1080")
-				qualstring = 'vq=hd1080';
-			else if (vidqual == "hd")
-				qualstring = 'vq=hd720';
-			else if (vidqual == "hq")
-				qualstring = 'vq=large';
-			else if (vidqual == "low")
-				qualstring = 'vq=small';
+		var vidsize = PersistObject.getPreference("videoEmbedSize");
+		var vidwidth, vidheight;
 
-			// Format for URL:
-			if (qualstring != '' && yt_start != '')
-				yt_start = '&' + yt_start;
-			if (qualstring != '' || yt_start != '')
-				qualstring = '?' + qualstring;
-				
-			var vidsize = PersistObject.getPreference("videoEmbedSize");
-			var vidwidth, vidheight;
+		if (vidsize == "gigantic")
+		{
+			vidwidth = 1280;
+			vidheight = 750;
+		}
+		else if (vidsize == "large")
+		{
+			vidwidth = 854;
+			vidheight = 510;
+		}
+		else if (vidsize == "medium")
+		{
+			vidwidth = 640;
+			vidheight = 390;
+		}
+		else if (vidsize == "small")
+		{
+			vidwidth = 560;
+			vidheight = 345;
+		}
+		else if (vidsize == "tiny")
+		{
+			vidwidth = 480;
+			vidheight = 300;
+		}
+		else //if (vidsize == "custom")
+		{
+			vidwidth = PersistObject.getPreference("videoEmbedCustomWidth");
+			vidheight = PersistObject.getPreference("videoEmbedCustomHeight");
+		}
 
-			if (vidsize == "gigantic")
-			{
-				vidwidth = 1280;
-				vidheight = 745;
-			}
-			else if (vidsize == "large")
-			{
-				vidwidth = 853;
-				vidheight = 505;
-			}
-			else if (vidsize == "medium")
-			{
-				vidwidth = 640;
-				vidheight = 385;
-			}
-			else if (vidsize == "small")
-			{
-				vidwidth = 560;
-				vidheight = 340;
-			}
-			else if (vidsize == "tiny")
-			{
-				vidwidth = 480;
-				vidheight = 295;
-			}
-			else //if (vidsize == "custom")
-			{
-				vidwidth = PersistObject.getPreference("videoEmbedCustomWidth");
-				vidheight = PersistObject.getPreference("videoEmbedCustomHeight");
-			}
+		//create the embedded elements (p containing video for linebreaky goodness)
+		var doc = e.originalTarget.ownerDocument;
+		var p = doc.createElement("p");
 
-			var embedFrame = doc.createElement("iframe");
-			embedFrame.setAttribute('width', vidwidth);
-			embedFrame.setAttribute('height', vidheight);
-			embedFrame.setAttribute('class', 'salr_video');
-			embedFrame.setAttribute('src', "http://" + yt_subd + "youtube.com/embed/" + videoId + qualstring + yt_start);
-			embedFrame.setAttribute('frameborder', '0');
-			embedFrame.setAttribute('mozallowfullscreen', true);
-			p.appendChild(embedFrame);
-			break;
+		switch (videoType)
+		{
+			case "webm":
+				var webmEmbed = doc.createElement("video");
+				webmEmbed.textContent = "ERROR! Something went wrong or your browser just can't play this video."
+				webmEmbed.setAttribute('src',link.href);
+				webmEmbed.setAttribute('type','video/webm');
+				webmEmbed.setAttribute('class', 'salr_video');
+				webmEmbed.setAttribute('width', vidwidth);
+				webmEmbed.controls = true;
+				p.appendChild(webmEmbed);
+				break;
+			case "gifv":
+				// Special code for imgur gifv - looping webm/mp4
+				var gifvEmbed = doc.createElement("video");
+				gifvEmbed.textContent = "ERROR! Something went wrong or your browser just can't play this video."
+				gifvEmbed.setAttribute('class', 'salr_video');
+				gifvEmbed.setAttribute('width', vidwidth);
+				gifvEmbed.setAttribute('poster',link.href.replace(/\.gifv$/i,'h.jpg'));
+				gifvEmbed.controls = true;
+				gifvEmbed.autoplay = true;
+				gifvEmbed.muted = true;
+				gifvEmbed.loop = true;
+				var gifvSource = doc.createElement("source");
+				gifvSource.setAttribute('src',link.href.replace(/\.gifv$/i,'.mp4'));
+				gifvSource.setAttribute('type','video/mp4');
+				gifvEmbed.appendChild(gifvSource);
+				gifvSource = doc.createElement("source");
+				gifvSource.setAttribute('src',link.href.replace(/\.gifv$/i,'.webm'));
+				gifvSource.setAttribute('type','video/webm');
+				gifvEmbed.appendChild(gifvSource);
+				p.appendChild(gifvEmbed);
+				break;
+			case "google":
+				var embed = doc.createElement("EMBED");
+				embed.setAttribute('width', 450);
+				embed.setAttribute('height', 370);
+				embed.setAttribute('type', "application/x-shockwave-flash");
+				embed.setAttribute('class', 'salr_video');
+				embed.setAttribute('id', videoId);
+				embed.setAttribute('flashvars', '');
+				embed.setAttribute('src', 'http://video.google.c' + videoTLD + '/googleplayer.swf?docId=' + videoId + '&hl=en&fs=true');
+				embed.setAttribute('allowfullscreen', "true");
+				p.appendChild(embed);
+				break;
+			case "youtube":
+				// Figure out quality and size to use
+				var vidqual = PersistObject.getPreference("videoEmbedQuality");
+				var qualstring = '';
+				if (vidqual == "hd1080")
+					qualstring = 'vq=hd1080';
+				else if (vidqual == "hd")
+					qualstring = 'vq=hd720';
+				else if (vidqual == "hq")
+					qualstring = 'vq=large';
+				else if (vidqual == "low")
+					qualstring = 'vq=small';
+
+				// Format for URL:
+				if (qualstring != '' && yt_start != '')
+					yt_start = '&' + yt_start;
+				if (qualstring != '' || yt_start != '')
+					qualstring = '?' + qualstring;
+
+				var embedFrame = doc.createElement("iframe");
+				embedFrame.setAttribute('width', vidwidth);
+				embedFrame.setAttribute('height', vidheight);
+				embedFrame.setAttribute('class', 'salr_video');
+				embedFrame.setAttribute('src', "http://" + yt_subd + "youtube.com/embed/" + videoId + qualstring + yt_start);
+				embedFrame.setAttribute('frameborder', '0');
+				embedFrame.setAttribute('mozallowfullscreen', true);
+				p.appendChild(embedFrame);
+				break;
+		}
+		//inserts video after the link
+		link.parentNode.insertBefore(p, link.nextSibling);
 	}
-
-	//inserts video after the link
-	link.parentNode.insertBefore(p, link.nextSibling);
 }
 
 function ReadFile(fn)
@@ -2242,8 +2294,10 @@ salrPersistObject.prototype = {
 			}
 
 			if (enableVideoEmbeds &&
-				(link.href.search(/^https?\:\/\/((?:www|[a-z]{2})\.)?(?:youtube\.com\/watch\?v=|youtu\.be\/)([-_0-9a-zA-Z]+)/i) > -1 ||
-				 link.href.search(/^http\:\/\/video\.google\.c(om|a|o\.uk)\/videoplay\?docid=([-0-9]+)/i) > -1))
+				((link.href.search(/^https?\:\/\/((?:www|[a-z]{2})\.)?(?:youtube\.com\/watch\?v=|youtu\.be\/)([-_0-9a-zA-Z]+)/i) > -1) ||
+				 (link.href.search(/^http\:\/\/video\.google\.c(om|a|o\.uk)\/videoplay\?docid=([-0-9]+)/i) > -1) ||
+				 link.href.match(/^https?\:\/\/(?:.)+\.(webm|gifv)$/i) ||
+				 link.href.match(/^https?\:\/\/i\.imgur\.com\/(?:.)+\.gifv$/i)))
 			{
 				link.style.backgroundColor = videoEmbedderBG;
 				link.addEventListener('click', SALR_vidClick, false);
