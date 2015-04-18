dbf29190e62f46e71ef0403d001c5e43e3a075c1
astral <astralsalr@gmail.com>
2015-04-17 22:43:30 -0700

    Enable our alternate forum cancer display by default

--- defaults/preferences/salastread.js
+++ defaults/preferences/salastread.js
@@ -43,7 +43,7 @@ pref('extensions.salastread.lastPostOnNavigator', true);
 pref('extensions.salastread.gestureEnable', false);
 pref('extensions.salastread.suppressErrors', true);
 pref('extensions.salastread.gestureButton', 2);
-pref('extensions.salastread.cancerTreatment', 0);
+pref('extensions.salastread.cancerTreatment', 1);
 pref('extensions.salastread.showForumModsList', false);
 
 //IMAGE HANDLING
