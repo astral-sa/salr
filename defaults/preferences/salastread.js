445cab5ff99e01087b1b69f7b6a971bafc3a267a
astral <astralsalr@gmail.com>
2015-05-23 00:24:57 -0700

    Increment version number for pre-release build 5

--- defaults/preferences/salastread.js
+++ defaults/preferences/salastread.js
@@ -1,4 +1,4 @@
-pref('extensions.salastread.currentVersion', '1.99.150521');
+pref('extensions.salastread.currentVersion', '1.99.150522');
 pref('extensions.salastread.disabled', false);
 pref('extensions.salastread.postsPerPage', 0);
 pref('extensions.salastread.enableFYAD', true);
