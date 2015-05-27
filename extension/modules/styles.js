d65dc6db2140c39741ad34802162476d50737d3b
astral <astralsalr@gmail.com>
2015-05-26 21:59:56 -0700

    Hide timgs in spoilers (work around forum bug) - closes #43

--- extension/modules/styles.js
+++ extension/modules/styles.js
@@ -381,6 +381,10 @@ let Styles = exports.Styles =
 			CSSFile += 'td.postbody .cancerous { opacity: 1; }\n';
 		}
 
+		// Hide timgs in spoilers - workaround forum bug
+		CSSFile += '.bbc-spoiler img.timg { visibility: hidden; }\n' +
+					'.bbc-spoiler:hover img.timg { visibility: visible; }\n';
+
 		// Remove forum-added blue border from our video embeds
 		CSSFile += 'td.postbody iframe.salr_video { border: none; }\n';
 
