b965083f2e83defb5aaa7274e5034f19fb5052af
astral <astralsalr@gmail.com>
2015-05-26 22:51:53 -0700

    Remove some duplication from the old forums prefpane. updates #55

--- extension/content/pref/pref-forums.js
+++ extension/content/pref/pref-forums.js
@@ -1,49 +1,20 @@
 function initForums()
 {
-	toggleNewReCount();
-	toggleUnvisitIcon();
-	toggleLastReadIcon();
-	togglePromoteStickiesInCP();
-}
+	toggleDependentPrefUI("newReCount","newReCountNewLine");
+	toggleDependentPrefUI("showThreadsWNPCP","promoteStickiesWNPCP");
+	toggleDependentPrefUI("toggleLastReadIcon","lastReadLabel","lastReadIcon","lastReadIconDefault");
+	toggleDependentPrefUI("toggleUnvisitIcon","unvisitLabel","unvisitIcon","unvisitIconDefault");
 
-function toggleNewReCount() {
-	if(document.getElementById("newReCount").checked && !document.getElementById("newReCount").disabled) {
-		document.getElementById("newReCountNewLine").disabled = false;
-	} else {
-		document.getElementById("newReCountNewLine").disabled = true;
-	}
-}
-
-function toggleLastReadIcon() {
-	if(document.getElementById("toggleLastReadIcon").checked) {
-		//enable the others
-		document.getElementById("lastReadIcon").disabled = false;
-		document.getElementById("lastReadIconDefault").disabled = false;
-	} else {
-		//disable
-		document.getElementById("lastReadIcon").disabled = true;
-		document.getElementById("lastReadIconDefault").disabled = true;
-	}
-}
-
-function toggleUnvisitIcon() {
-	if(document.getElementById("toggleUnvisitIcon").checked) {
-		//enable
-		document.getElementById("unvisitIcon").disabled = false;
-		document.getElementById("unvisitIconDefault").disabled = false;
-	} else {
-		//disable
-		document.getElementById("unvisitIcon").disabled = true;
-		document.getElementById("unvisitIconDefault").disabled = true;
-	}
-}
-
-function togglePromoteStickiesInCP()
-{
-	if (document.getElementById("showThreadsWNPCP").checked && !document.getElementById("showThreadsWNPCP").disabled)
-		document.getElementById("promoteStickiesWNPCP").disabled = false;
-	else
-		document.getElementById("promoteStickiesWNPCP").disabled = true;
+	setEventListener("newReCount", "command", function() {
+		toggleDependentPrefUI("newReCount","newReCountNewLine");});
+	setEventListener("showThreadsWNPCP", "command", function() {
+		toggleDependentPrefUI("showThreadsWNPCP","promoteStickiesWNPCP");});
+	setEventListener("toggleLastReadIcon", "command", function() {
+		toggleDependentPrefUI("toggleLastReadIcon","lastReadLabel","lastReadIcon","lastReadIconDefault");});
+	setEventListener("toggleUnvisitIcon", "command", function() {
+		toggleDependentPrefUI("toggleUnvisitIcon","unvisitLabel","unvisitIcon","unvisitIconDefault");});
+	setEventListener("lastReadIconDefault", "command", lastReadIconDefault);
+	setEventListener("unvisitIconDefault", "command", unvisitIconDefault);
 }
 
 function lastReadIconDefault() {
